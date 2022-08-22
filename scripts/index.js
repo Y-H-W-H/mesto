e1d8d48b7041f3da088f78627f54802886424097
bob <efimsirin@yandex.ru>
2022-08-22 22:17:14 +0300

    1sr-review-fixes

---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+let editButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let form = popup.querySelector('.popup__form');
+let closeButton = popup.querySelector('.popup__close-button');
+let popupName = popup.querySelector('#popupname');
+let popupAbout = popup.querySelector('#popupabout');
+let profileName = document.querySelector('.profile__name');
+let profileAbout = document.querySelector('.profile__about');
+
+
+function openPopup() {
+  popup.classList.toggle('popup_opened');
+};
+
+function closePopup() {
+  popup.classList.remove('popup_opened');
+  popup.classList.add('popup_invisible');
+
+};
+
+function editProfile(event) {
+  event.preventDefault();
+  profileName.textContent = popupName.value;
+  profileAbout.textContent = popupAbout.value;
+  closePopup();
+
+};
+closeButton.addEventListener('click', closePopup);
+editButton.addEventListener('click', openPopup);
+form.addEventListener('submit', editProfile);
