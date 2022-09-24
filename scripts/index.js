70a27f4becd4ad79a284b38061c3111db3a6a393
Y-H-W-H <103368479+Y-H-W-H@users.noreply.github.com>
2022-09-24 14:44:09 +0300

    gg

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,173 @@
-let editButton = document.querySelector('.profile__edit-button');
+
+
+let profileFrame = document.querySelector('.profile')
+let popupProfile = document.querySelector('.popup_profile');
 let popup = document.querySelector('.popup');
-let form = popup.querySelector('.popup__form');
-let closeButton = popup.querySelector('.popup__close-button');
-let popupName = popup.querySelector('#popupname');
-let popupAbout = popup.querySelector('#popupabout');
-let profileName = document.querySelector('.profile__name');
-let profileAbout = document.querySelector('.profile__about');
+let closeProfileBtn = popupProfile.querySelector('.popup__close-button_profile');
+let editProfileBtn = document.querySelector('.profile__edit-button');
+let profileName = profileFrame.querySelector('.profile__name');
+let profileAbout = profileFrame.querySelector('.profile__about');
+let profileForm = popupProfile.querySelector('.popup__form_profile');
+//==========ADD-CARD-TOOLS
 
 
-function openPopup() {
-  popup.classList.toggle('popup_opened');
-  popupAbout.value = profileAbout.textContent;
-  popupName.value = profileName.textContent;
+
+let popupCard = document.querySelector('.popup_card');
+let openCard = document.querySelector('.open-card');
+let AllImages = document.querySelectorAll('.elements__image');
+let openCardImage = openCard.querySelector('.open-card__image');
+let openCardClose = openCard.querySelector('.open-card__close ');
+let openCardHeader = openCard.querySelector('.open-card__header');
+
+openCardClose.addEventListener('click', function () { openCard.classList.remove('open-card_active'); });
+AllImages.forEach((c) => {
+  c.addEventListener('click', function (i) {
+    let gG = i.target.closest('.elements__card');
+    // console.log(gG);
+    console.log(gG.querySelector('.elements__image').src);
+    console.log(gG.querySelector('.elements__header').textContent);
+    openCardImage.src = gG.querySelector('.elements__image').src;
+    openCardHeader.textContent = gG.querySelector('.elements__header').textContent;
+    openCard.classList.add('open-card_active');
+  }
+  )
+});
+
+
+let closeCardBtn = popupCard.querySelector('.popup__close-button_card');
+let addCardBtn = document.querySelector('.profile__add-button');
+let cardTitle = popupCard.querySelector('.popup__input_title');
+let cardLink = popupCard.querySelector('.popup__input_link');
+let cardForm = popupCard.querySelector('.popup__form_card');
+const cardTemplate = document.querySelector('#card-template');
+let cardsList = document.querySelector('.elements__list');
+let likeBtns = Array.from(cardsList.querySelectorAll('.elements__like'));
+let cardBottom = Array.from(cardsList.getElementsByClassName('elements__bottom'));
+const trashBtn = cardTemplate.content.querySelector('.elements__trash-btn');
+//==========6 BASIC-CARDS-DIPLAY
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://images.unsplash.com/photo-1663417026017-dd3933e19074?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMnx8fGVufDB8fHx8&w=1000&q=80'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://images.unsplash.com/photo-1663474830359-4b622189c6ac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8&w=1000&q=80'
+  },
+  {
+    name: 'Pramod tiwari',
+    link: 'https://images.unsplash.com/photo-1663792804509-26c136d5f7a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=825&q=80'
+  },
+  {
+    name: 'Роза роз',
+    link: 'https://images.unsplash.com/photo-1663848282259-60dda6019692?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzN3x8fGVufDB8fHx8&w=1000&q=80'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://images.unsplash.com/photo-1657214059139-dc58d16118ed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwzNXx8fGVufDB8fHx8&w=1000&q=80'
+  }
+];
+let cardImages = document.querySelectorAll('.elements__image');
+
+for (i = 0; i < initialCards.length; ++i) {
+
+  cardImages[i].setAttribute("src", initialCards[i].link);
+
+};
+
+
+
+
+
+
+
+
+function toggleProfile() {
+
+  popupProfile.classList.toggle('popup_opened');
 
 }
-
-function closePopup() {
-  popup.classList.remove('popup_opened');
-
-}
-
-function editProfile(event) {
+function submitProfileInfo(event) {
   event.preventDefault();
-  profileName.textContent = popupName.value;
-  profileAbout.textContent = popupAbout.value;
-  closePopup();
+  profileName.textContent = popupProfile.querySelector('.popup__input_name').value;
+  profileAbout.textContent = popupProfile.querySelector('.popup__input_about').value;
+  toggleProfile();
+
+
 
 }
-closeButton.addEventListener('click', closePopup);
-editButton.addEventListener('click', openPopup);
-form.addEventListener('submit', editProfile);
+editProfileBtn.addEventListener('click', toggleProfile);
+closeProfileBtn.addEventListener('click', toggleProfile);
+profileForm.addEventListener('submit', submitProfileInfo);
+
+
+
+
+
+
+
+
+
+function toggleCard() {
+  popupCard.classList.toggle('popup_opened');
+
+}
+addCardBtn.addEventListener('click', toggleCard)
+closeCardBtn.addEventListener('click', toggleCard);
+cardForm.addEventListener('submit', submitCardInfo);
+
+cardBottom.forEach((element) => {
+
+  element.prepend(trashBtn.cloneNode(true));
+});
+
+let trashBtns = Array.from(cardsList.querySelectorAll('.elements__trash-btn'));
+function submitCardInfo(event) {
+  event.preventDefault();
+  cardsList.prepend(cardTemplate.content.cloneNode(true));
+  let cardsItem = cardsList.querySelector('.elements__card');
+  cardsItem.querySelector('.elements__image').src = cardLink.value;
+  cardsItem.querySelector('.elements__header').textContent = cardTitle.value;
+  cardsItem.querySelector('.elements__trash-btn').onclick = delCard;
+  heartBtn = cardsItem.querySelector('.elements__like')
+  heartBtn.addEventListener('click', function (evt) {
+    evt.target.classList.toggle('elements__like_active');
+  })
+  likeBtns.unshift(heartBtn);
+
+  toggleCard();
+  console.log(trashBtns);
+
+
+}
+function delCard(ev) {
+  console.log(ev.target);
+  ev.target.closest('.elements__card').remove();
+}
+
+
+trashBtns.forEach((tb) => { tb.addEventListener('click', delCard) });
+
+
+
+
+
+
+likeBtns.forEach((like) => {
+  like.onclick = function () {
+    like.classList.toggle('elements__like_active');
+  }
+});
+
+
+
+
+
+
+
+
+
