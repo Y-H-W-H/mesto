e5bd64382ea7b296392a3b78010acc9eb8041fb6
Y-H-W-H <103368479+Y-H-W-H@users.noreply.github.com>
2022-09-27 23:19:46 +0300

    latest code-review fixes
    More optimal logic. More relevant order. Fixing incorrect or weak decisions.

--- scripts/index.js
+++ scripts/index.js
@@ -2,113 +2,110 @@
 // РАЗДЕЛ 1. ОБЪЯВЛЕНИЕ ГЛОБАЛЬНЫХ ПЕРЕМЕННЫХ И КОНСТАНТ
 // ===========================================================
 
-
 //1.1. Инструментарий для работы с профилем
 const profileFrame = document.querySelector('.profile')
 const popupProfile = document.querySelector('.popup_profile');
-const popup = document.getElementById('popupE');
-const profileCloseBtn = popupProfile.querySelector('.popup__close-button_profile');
-const editProfileBtn = document.querySelector('.profile__edit-button');
+const profileEditionBtn = document.querySelector('.profile__edit-button');
 const profileName = profileFrame.querySelector('.profile__name');
 const profileAbout = profileFrame.querySelector('.profile__about');
 const profileForm = popupProfile.querySelector('.popup__form_profile');
-//1.2. Инструментарий для работы с карточками
-const popupCard = document.querySelector('.popup_card');
-const closeCardBtn = popupCard.querySelector('.popup__close-button_card');
-const addCardBtn = document.querySelector('.profile__add-button');
-const cardTitle = popupCard.querySelector('.popup__input_title');
-const cardLink = popupCard.querySelector('.popup__input_link');
-const cardForm = popupCard.querySelector('.popup__form_card');
+const profileInputName = popupProfile.querySelector('.popup__input_name');
+const profileInputAbout = popupProfile.querySelector('.popup__input_about');
+//1.2. Инструментарий для добавления карточек
+const popupAddCard = document.querySelector('.popup_addCard');
+// Ниже в качестве компромисного варианта выбран 'adding' в качестве герундива.
+// Т.о. он указывает на 'добавление' как на сущность, и, стало быть, выступает в роли существительного
+const addingCardButton = document.querySelector('.profile__add-button');
+const cardTitle = popupAddCard.querySelector('.popup__input_title');
+const cardLink = popupAddCard.querySelector('.popup__input_link');
+const cardForm = popupAddCard.querySelector('.popup__form_card');
 const cardTemplate = document.querySelector('#card-template');
-const cardsList = document.querySelector('.elements__list');
-const likeBtns = Array.from(cardsList.querySelectorAll('.elements__like'));
-const cardBottom = Array.from(cardsList.getElementsByClassName('elements__bottom'));
-const trashBtns = Array.from(cardsList.querySelectorAll('.elements__trash-btn'));
+const cardsContainer = document.querySelector('.elements__list');
 // 1.3. Звенья, обеспечивающие работу просмотра картинок
-const cardScaled = document.querySelector('.card-scaled');
-const cardScaledImage = cardScaled.querySelector('.card-scaled__image');
-const cardScaledClose = cardScaled.querySelector('.card-scaled__close');
-const cardScaledHeader = cardScaled.querySelector('.card-scaled__header');
+const popupCardScaled = document.querySelector('.popup_card-scaled');
+const popupCardScaledImage = popupCardScaled.querySelector('.popup_card-scaled__image');
+const popupCardScaledCloseButton = popupCardScaled.querySelector('.popup_card-scaled__close-button');
+const popupCardScaledHeader = popupCardScaled.querySelector('.popup_card-scaled__header');
+// 1.4. Сбор коллекции закрывающих попап кнопок
+const popupClosureButtons = document.querySelectorAll('.popup__close-button');
 
 
 
 // РАЗДЕЛ 2. ОПИСАНИЕ ФУНКЦИЙ, РЕАЛИЗУЮЩИХ КОНТЕНТ СТРАНИЦЫ
 // ===========================================================
-//2.1. Функции, обрабатывающие профиль
-function toggleProfile() {
-  popupProfile.classList.toggle('popup_opened');
-  popupProfile.querySelector('.popup__input_name').value = '';
-  popupProfile.querySelector('.popup__input_about').value = '';
-}
+
+//2.1. Функция подтверждения профиля
 function submitProfileInfo(event) {
   event.preventDefault();
-  profileName.textContent = popupProfile.querySelector('.popup__input_name').value;
-  profileAbout.textContent = popupProfile.querySelector('.popup__input_about').value;
-  toggleProfile();
-
-
-
+  profileName.textContent = profileInputName.value;
+  profileAbout.textContent = profileInputAbout.value;
+  wrapPopup(event);
+}
+//2.2. Универсальные открывашка и закрывашка
+function wrapPopup(c) {
+  c.target.closest('.popup').classList.remove('popup_opened');
+}
+function unwrapPopup(o) {
+  switch (o.target) {
+    case profileEditionBtn: popupProfile.classList.add('popup_opened');
+      break;
+    case addingCardButton: popupAddCard.classList.add('popup_opened');
+      break;
+    default: popupCardScaled.classList.add('popup_opened');
+  }
 }
 
 
 
 // РАЗДЕЛ 3. ПРОПИСЫВАНИЕ ФУНКЦИОНАЛА ДЛЯ ГЕНЕРАЦИИ И РЕНДЕРИНГА КАРТОЧЕК
 // ======================================================================
-// Общая генеративная функция для карточек
-function cardsCreation(l, v) {
+
+//3.1. Общая генеративная функция для карточек
+function createCard(header, link) {
   // Взятие узла карточки с его HTML-содержимым
-  let newCardNode = cardTemplate.content.cloneNode(true);
-  let newCardItem = newCardNode.querySelector('.elements__card');
-  let newCardImage = newCardItem.querySelector('.elements__image');
-  let newCardHeader = newCardItem.querySelector('.elements__header');
+  const newCardNode = cardTemplate.content.cloneNode(true);
+  const newCardItem = newCardNode.querySelector('.elements__card');
+  const newCardImage = newCardItem.querySelector('.elements__image');
+  const newCardHeader = newCardItem.querySelector('.elements__header');
   // Насыщение будущей карточки контентом
-  newCardImage.src = v;
-  newCardHeader.textContent = l;
+  newCardImage.src = link;
+  newCardHeader.textContent = header;
   newCardImage.alt = newCardHeader.textContent;
   //Набрасывание слушателей на соответствующие элементы карточки
-  let trashBtn = newCardItem.querySelector('.elements__trash-btn');
-  trashBtn.onclick = delCard;
-  let heartBtn = newCardItem.querySelector('.elements__like');
+  const trashBtn = newCardItem.querySelector('.elements__trash-btn');
+  trashBtn.addEventListener('click', deleteCard);
+  const heartBtn = newCardItem.querySelector('.elements__like');
   heartBtn.addEventListener('click', function (evt) {
     evt.target.classList.toggle('elements__like_active');
   });
   // Создание формы просмотра каждой генерируемой карточки при клике на картинку
   newCardImage.addEventListener('click', function (clicked) {
-    let cardToOpen = clicked.target.closest('.elements__card');
-    console.log(cardToOpen);
-    cardScaledImage.src = cardToOpen.querySelector('.elements__image').src;
-    cardScaledHeader.textContent = cardToOpen.querySelector('.elements__header').textContent;
-    cardScaled.classList.add('card-scaled_active');
+    const cardToOpen = clicked.target.closest('.elements__card');
+    popupCardScaledImage.src = cardToOpen.querySelector('.elements__image').src;
+    popupCardScaledHeader.textContent = cardToOpen.querySelector('.elements__header').textContent;
+    unwrapPopup(popupCardScaled);
   });
-  toggleCard();
-  prependNewCard(newCardNode);
+  return newCardNode;
 }
-// Функция добавления уже насыщенной карточки в документ разметки
-function prependNewCard(x) {
-  cardsList.prepend(x);
-  console.log(cardsList);
+//3.2. Функция добавления уже насыщенной карточки в документ разметки
+function prependCard(cardNode) {
+  cardsContainer.prepend(cardNode);
 }
-//Частная функция, порождающая произвольные карточки по запросу пользователя
-function createNewCard(event) {
+//3.3. Частная функция, порождающая произвольные карточки по запросу пользователя
+function submitCard(event) {
   event.preventDefault();
-  cardsCreation(cardTitle.value, cardLink.value);
+  prependCard(createCard(cardTitle.value, cardLink.value));
+  wrapPopup(event);
 }
-//  Механизм, реализующий открытие/закрытие add-card-интерфейса
-function toggleCard() {
-  popupCard.classList.toggle('popup_opened');
-  popupCard.querySelector('.popup__input_title').value = '';
-  popupCard.querySelector('.popup__input_link').value = '';
-
-}
-// Функция удаления карточки через событие клика по удаляющей кнопке
-function delCard(ev) {
+//3.4. Функция удаления карточки через событие клика по удаляющей кнопке
+function deleteCard(ev) {
   ev.target.closest('.elements__card').remove();
 }
-// Перебор масс. исх. карточек, итеративно взимаются и реализуются их дефолтные данные
-initialCards.forEach((a) => {
-  let initLink = a.link;
-  let initTitle = a.name;
-  cardsCreation(initTitle, initLink);
+//3.5. Перебор масс. исх. карточек, итеративно взимаются и реализуются их дефолтные данные
+initialCards.forEach((card) => {
+  const initLink = card.link;
+  const initTitle = card.name;
+  prependCard(createCard(initTitle, initLink));
 })
 
 
@@ -117,20 +114,13 @@ initialCards.forEach((a) => {
 
 // РАЗДЕЛ 4. УСТАНОВКА ГЛОБАЛЬНЫХ СЛУШАТЕЛЕЙ
 // =====================================================
-// Слушатель событий на элементы профиля
-editProfileBtn.addEventListener('click', toggleProfile);
-profileCloseBtn.addEventListener('click', toggleProfile);
+//4.1. Слушатель событий на элементы профиля
+profileEditionBtn.addEventListener('click', unwrapPopup);
 profileForm.addEventListener('submit', submitProfileInfo);
-
-// Слушатель событий на элементы добавления карточек
-addCardBtn.addEventListener('click', toggleCard)
-closeCardBtn.addEventListener('click', toggleCard);
-cardForm.addEventListener('submit', createNewCard);
-
-
-//Закрепление слушателя на кнопке закрытия просмотра карточки
-cardScaledClose.addEventListener('click', function () {
-  cardScaled.classList.remove('card-scaled_active');
-});
+//4.2. Слушатель событий на элементы добавления карточек
+addingCardButton.addEventListener('click', unwrapPopup)
+cardForm.addEventListener('submit', submitCard);
+//4.3. Закрепление слушателя на кнопке закрытия просмотра карточки
+Array.from(popupClosureButtons).forEach((t) => t.addEventListener('click', wrapPopup));
 
 
