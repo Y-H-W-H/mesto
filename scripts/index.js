2430a27265cdb85f4c3de3069ccce50c65bc3237
Y-H-W-H <103368479+Y-H-W-H@users.noreply.github.com>
2022-11-21 16:02:48 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,9 @@
+
+//Nessesary imports
 import { Card } from './Card.js';
 import { FormValidator } from './FormValidator.js';
 // ===========================================================
+// Initial card-list
 const cardList = [
   {
     name: 'Архыз',
@@ -28,7 +31,7 @@ const cardList = [
   },
 
 ];
-// Здесь пребывают те данные и функционал, реализующие валидацию форм
+// Здесь пребывают данные, реализующие валидацию форм
 const formSettings = {
   formSelector: '.popup__form',
   inputSelector: '.popup__input',
@@ -40,7 +43,7 @@ const formSettings = {
   errorMessage: "А ну-ка заполни правильно"
 
 }
-//1.1. Инструментарий для работы с профилем
+// Инструментарий для работы с профилем
 const profileFrame = document.querySelector(".profile");
 const profileEditionBtn = document.querySelector(".profile__edit-button");
 const profileName = profileFrame.querySelector(".profile__name");
@@ -49,9 +52,6 @@ const profileAbout = profileFrame.querySelector(".profile__about");
 const popupProfile = document.querySelector(".popup_profile");
 const popupCardScaled = document.querySelector(".popup_scaled");
 
-const popupProfileCloseButton = popupProfile.querySelector(
-  ".popup__close-button_profile"
-);
 const profileForm = popupProfile.querySelector(".popup__form_profile");
 const profileInputName = popupProfile.querySelector(".popup__input_name");
 const profileInputAbout = popupProfile.querySelector(".popup__input_about");
@@ -63,8 +63,7 @@ const popupAddCard = document.querySelector('.popup_add-card');
 const cardForm = popupAddCard.querySelector('.popup__form_card');
 
 const addingCardButton = document.querySelector(".profile__add-button");
-const popupAddCardCloseButton = popupAddCard.querySelector(".popup__close-button_add-card");
-const popupCardScaledCloseButton = popupCardScaled.querySelector(".popup__close-button_scaled");
+
 
 const forms = Array.from(document.querySelectorAll(formSettings.formSelector));
 const popups = document.querySelectorAll('.popup');
@@ -85,17 +84,16 @@ profileInputAbout.value = profileAbout.textContent;
 function openPopup(popUp) {
   popUp.classList.add('popup_opened');
   window.addEventListener('keydown', onPressEsc);
-  window.addEventListener('click', onClickOverlay);
 
 }
-
+//Функция закрытия любого поп-апа
 function closePopup(popUp) {
   popUp.classList.remove('popup_opened');
   window.removeEventListener('keydown', onPressEsc);
-  window.removeEventListener('click', onClickOverlay)
+
 }
 
-
+//Listening to popups closure
 popups.forEach((popup) => popup.addEventListener('mousedown', (event) => {
 
   if (event.target.classList.contains('popup_opened')) {
@@ -107,6 +105,30 @@ popups.forEach((popup) => popup.addEventListener('mousedown', (event) => {
 
 }))
 
+//Creating a card to prepend
+function createCard(data) {
+  const cardObject = new Card(data, '#card-template', handleCardClick);
+  const card = cardObject.generateCard();
+  return card;
+}
+//Prepending the card created depending
+function prependCard(data) {
+  cardListContainer.prepend(createCard(data));
+}
+
+//Функция добавления пользователькой карточки, использующая пользовательские инпуты в качестве массива для postCard
+function submitCard(evt) {
+  evt.preventDefault();
+  prependCard({
+    name: cardTitle.value,
+    link: cardLink.value
+  });
+  cardForm.reset();
+  closePopup(popupAddCard);
+}
+
+//Posting initial card-list
+cardList.forEach((cardListItem) => prependCard(cardListItem));
 
 
 //Функция подтверждения профиля
@@ -117,16 +139,9 @@ function submitProfileInfo(event) {
   closePopup(popupProfile);
 }
 
-//Общая генеративная функция для карточек
-function postCard(dataMassive) {
-  dataMassive.forEach((data) => {
-    const cardObject = new Card(data, '#card-template', handleCardClick);
-    const card = cardObject.generateCard();
-    cardListContainer.prepend(card);
-  });
 
-}
 
+//scale post-image
 function handleCardClick(name, link) {
   scaledImage.src = link;
   scaledImageHeader.alt = name;
@@ -135,17 +150,7 @@ function handleCardClick(name, link) {
 }
 
 
-//Функция добавления пользователькой карточки, использующая пользовательские инпуты в качестве массива для postCard
-function submitCard(evt) {
-  evt.preventDefault();
 
-  postCard([{
-    name: cardTitle.value,
-    link: cardLink.value
-  }])
-  cardForm.reset();
-  closePopup(popupAddCard);
-}
 
 //Функция закрытия поп-апа по клавише ESC
 function onPressEsc(event) {
@@ -155,51 +160,34 @@ function onPressEsc(event) {
     closePopup(popup);
   }
 }
-//Функция закрытия поп-апа по клику на оверлэй
-function onClickOverlay(event) {
-  if (!event.target.classList.contains("popup")) return;
-  const popup = event.target.closest(".popup");
-  if (!popup) return;
-  closePopup(popup);
-}
-
-//Вызов общей генеративной функции для исходного массива карточек
-postCard(cardList);
-
-
-
 
 
 // //Включаем валидацию всех форм
-forms.forEach((form) => {
-  const thisFormValidator = new FormValidator(formSettings, form);
-  thisFormValidator.enableValidation();
-  return thisFormValidator;
-})
+
+const profileFormValidator = new FormValidator(formSettings, profileForm);
+const cardFormValidator = new FormValidator(formSettings, cardForm);
+profileFormValidator.enableValidation();
+cardFormValidator.enableValidation();
 
 
 
 
 
+//Listening to profile and card submission
+profileForm.addEventListener("submit", (evt) => submitProfileInfo(evt));
+cardForm.addEventListener("submit", (event) => submitCard(event));
+
+
+
+//Listening to profile edition and card creation
+addingCardButton.addEventListener("click", function () {
+  cardFormValidator.resetValidation();
+  openPopup(popupAddCard);
+});
 //______________________________________________________
 profileEditionBtn.addEventListener("click", function () {
-  const validityObject = new FormValidator(formSettings, profileForm);
-  validityObject.resetValidation();
+  profileFormValidator.resetValidation();
   openPopup(popupProfile);
-
-});
-profileForm.addEventListener("submit", (evt) => {
-  submitProfileInfo(evt)
-});
-addingCardButton.addEventListener("click", function () {
-  const valObj = new FormValidator(formSettings, cardForm);
-  valObj.resetValidation();
-  openPopup(popupAddCard);
-  cardForm.addEventListener("submit", (event) => submitCard(event));
-
-
-
-
 });
 
 
